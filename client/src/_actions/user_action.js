7e5bd56924322583ab9e6a076e1e908231b30b6d
Young Hun <lee0h.0417@gmail.com>
2020-10-18 17:02:45 +0900

    ADD: 회원가입 페이지 구현

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, REGISTER_USER } from './types';
 export function loginUser(dataTosubmit) {
     const request = axios.post('http://localhost:5000/api/users/login', dataTosubmit)
     .then(res => res.data)
 
     return {
         type: LOGIN_USER,
+        payload: request
+    }
+}
+
+export function registerUser(dataTosubmit) {
+    const request = axios.post('http://localhost:5000/api/users/register', dataTosubmit)
+    .then(res => res.data)
+
+    return {
+        type: REGISTER_USER,
         payload: request
     }
 }
--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,10 +1,11 @@
-import { LOGIN_USER } from '../_actions/types';
+import { LOGIN_USER, REGISTER_USER } from '../_actions/types';
 
 export default function (state = {}, aciton) {
     switch (aciton.type) {
         case LOGIN_USER:
             return { ...state, loginSuccess: aciton.payload }
-            
+        case REGISTER_USER:
+            return { ...state, register: aciton.payload }
         default:
             return state;
     }
